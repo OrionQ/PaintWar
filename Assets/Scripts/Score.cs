99a409c5a0bb941c1765bc1e0d08e9c8f9d075ec
Orion Qin <xiaolei_qin@outlook.com>
2020-02-02 22:54:12 -0500

    Changed name

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+    public static int PlayerScore = 0;
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("particle hit");
+        transform.GetComponent<MeshRenderer>().material.color = other.GetComponent<ParticleSystemRenderer>().material.color;
+        PlayerScore++;
+        
+    }
+}
