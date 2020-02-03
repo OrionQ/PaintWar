ac3bf8706c2a870fa498c21208237463a4178575
Orion Qin <xiaolei_qin@outlook.com>
2020-02-02 21:29:44 -0500

    Left for color change of the spikes and count scores

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("particle hit");
+        transform.GetComponent<Renderer>().material.color = other.GetComponent<ParticleSystemRenderer>().material.color;
+        
+        
+    }
+}
---
+++ Assets/Scripts/RotateTurret.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTurret : MonoBehaviour
+{
+    public enum ROTATION_AXIS
+    {
+        xAxis,
+        yAxis,
+        zAxis
+    }
+    public float maxRotationAngle;
+    public float period;
+
+    float myTime;
+    public ROTATION_AXIS rotationAxis;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotationAxis == ROTATION_AXIS.xAxis)
+        {
+            myTime += Time.deltaTime;
+            float phase = Mathf.Sin(myTime / period);
+            transform.localRotation = Quaternion.Euler(new Vector3(phase * maxRotationAngle, 0, 0));
+        } else if (rotationAxis == ROTATION_AXIS.yAxis)
+        {
+            myTime += Time.deltaTime;
+            float phase = Mathf.Sin(myTime / period);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, phase * maxRotationAngle, 0));
+        } else if (rotationAxis == ROTATION_AXIS.zAxis)
+        {
+            myTime += Time.deltaTime;
+            float phase = Mathf.Sin(myTime / period);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, phase * maxRotationAngle));
+        }
+    }
+}
